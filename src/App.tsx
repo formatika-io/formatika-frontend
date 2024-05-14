405c2522429d232d660f20ee982a31b24e013ffd
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-05-14 10:43:15 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+
+function App() {
+  return (
+    <div className="text-3xl text-red-500">
+      Formatika is open soure project fo volunteers developers
+<FontAwesomeIcon icon={faArrowRight} className="text-2xl text-red-500" />
+
+    </div>
+  )
+}
+
+export default App
