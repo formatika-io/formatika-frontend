bf18604d5c40358a569726e40d989c55ae023323
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-05-15 10:39:52 +0400

    Add Navbar component with navigation links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-gray-800 text-white flex items-center justify-between px-4 py-2 shadow-md">
+      <Link to="/" className="text-xl font-bold">
+        Logo
+      </Link>
+      <ul className="flex space-x-4">
+        <li>
+          <Link to="/" className="hover:text-gray-400">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="hover:text-gray-400">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:text-gray-400">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
