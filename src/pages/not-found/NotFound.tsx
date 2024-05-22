3d22a2a964029b98a18d7da0784e4a2b147de190
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-05-22 17:07:09 +0400

    Update text color of NotFound page

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -4,16 +4,14 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div className="container mx-auto px-4 py-16 text-center">
+    <div className="container mx-auto px-4 py-16 text-center text-white">
       <FontAwesomeIcon
         icon={faExclamationTriangle}
         size="3x"
-        className="text-gray-800 mb-4"
+        className="mb-4"
       />
-      <h1 className="text-3xl font-bold text-gray-800">404: Not Found</h1>
-      <p className="text-gray-600 mt-4">
-        The page you requested could not be found.
-      </p>
+      <h1 className="text-3xl font-bold">404: Not Found</h1>
+      <p className="mt-4">The page you requested could not be found.</p>
       <Link to="/" className="text-blue-500 hover:underline mt-8 inline-block">
         Come back Home
       </Link>
