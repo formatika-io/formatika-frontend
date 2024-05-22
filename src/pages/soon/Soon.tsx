c0449bd00077f5073daf627bfcaf57eb1ff93ce2
RaminMikayilov <raminmikayilov2003@gmail.com>
2024-05-22 17:06:09 +0400

    Update text color of soon page

--- src/pages/soon/Soon.tsx
+++ src/pages/soon/Soon.tsx
@@ -4,16 +4,10 @@ import { Link } from "react-router-dom";
 
 function Soon() {
   return (
-    <div className="container mx-auto px-4 py-16 text-center">
-      <FontAwesomeIcon
-        icon={faClock}
-        size="3x"
-        className="text-gray-800 mb-4"
-      />
-      <h1 className="text-3xl font-bold text-gray-800">
-        This page is coming soon!
-      </h1>
-      <p className="text-gray-600 mt-4">
+    <div className="container mx-auto px-4 py-16 text-center text-white">
+      <FontAwesomeIcon icon={faClock} size="3x" className="mb-4" />
+      <h1 className="text-3xl font-bold">This page is coming soon!</h1>
+      <p className="mt-4">
         We're currently working on this section and will have it available soon.
       </p>
       <Link to="/" className="text-blue-500 hover:underline mt-8 inline-block">
